d8d93ce930f9b8fea56358277a299827ea8aca0d
pakhandi <asimkprasad@gmail.com>
2016-04-02 03:19:10 +0530

    Try rotating image

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ def playGame():
 
 	walls = []
 
+	playerCoordinate = []
+	playerCursor = []
+	playerOrientation = [0 for i in range(2)]
+	playerCoordinate.append((100,100))
+
 	for i in xrange(200):
 		walls.append((width / 2,i))
 
@@ -36,10 +41,14 @@ def playGame():
 
 	# circle(Surface, color, pos, radius, width=0)
 
+	playerCursor.append(pygame.image.load('tr.jpg'))
+	# playerCursor = pygame.transform.rotate(playerCursor, 270)
+
+	# screen.blit(playerCursor, (100, 100) )
 
 	for center in centers:
 		pygame.draw.circle(screen, GREY, center, 40, 0 )
-	# pygame.draw.circle(screen, GREY, (60,60), 40, 0)
+	# pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
 
 	for wall in walls:
 		pygame.draw.rect( screen, BROWN, [wall[0],wall[1],10,10] )
@@ -52,6 +61,15 @@ def playGame():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit(0)
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_a:
+					playerOrientation[0] = (playerOrientation[0] + 90) % 360
+					playerCursor[0] = pygame.transform.rotate(playerCursor[0], playerOrientation[0])
+				elif event.key == pygame.K_s:
+					playerOrientation[0] = (playerOrientation[0] - 90) % 360
+					playerCursor[0] = pygame.transform.rotate(playerCursor[0], playerOrientation[0])
+		screen.blit(playerCursor[0], (100,100) )
+		pygame.display.flip()
 
 def main():
 	playGame()
