dd9613956a903c6a74f0fc2df2c17cef81df9330
pakhandi <asimkprasad@gmail.com>
2016-04-02 10:13:13 +0530

    Add bullet motion

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import pygame
 import sys
 import math
+from copy import deepcopy
+
+class Bullet:
+
+	def __init__(self):
+		timeTravelled = 0
+		orientation = 0
+		coordinate = (0,0)
+		firedBy = -1
+		# return self
+
+def getDirection(orientation):
+	deg = orientation
+	x = +1
+	y = -1
+
+	if orientation > 90 and orientation <= 180:
+		deg = 180 - orientation
+		x = -1
+		y = -1
+	elif orientation > 180 and orientation<= 270:
+		deg = orientation - 180
+		x = -1
+		y = +1
+	elif orientation > 270 and orientation <= 360:
+		deg = 360 - orientation
+		x = +1
+		y = +1
+
+	return deg, x, y
 
 width = 1200
 height = 700
@@ -8,11 +38,14 @@ height = 700
 BLACK = (0, 0, 0)
 BROWN = (240,150,0)
 GREY = (128,128,128)
+WHITE = (255,255,255)
 
 def playGame(numberOfPlayers):
 	screen = pygame.display.set_mode((width, height))
 	pygame.display.set_caption("Castle Defence")
 	screen.fill(BLACK)
+
+	bullets = []
 
 	# centers for the towers
 	centers = []
@@ -73,6 +106,8 @@ def playGame(numberOfPlayers):
 	isRunning = True
 	clock = pygame.time.Clock()
 
+	bulletDistance = 0.8
+
 	unitDistance = 0.3
 
 	# r = pygame.draw.rect(screen, BROWN, [200, 200, 20 , 10] )
@@ -106,32 +141,62 @@ def playGame(numberOfPlayers):
 				elif event.key == pygame.K_k:
 					playerOrientation[3] = ( (playerOrientation[3] - 5) + 360 ) % 360
 					newPlayerCursor[3] = pygame.transform.rotate(playerCursor[3], playerOrientation[3])
+				elif event.key == pygame.K_z:
+					temp = Bullet()
+					temp.firedBy = 0
+					temp.timeTravelled = 0
+					temp.orientation = playerOrientation[0]
+					temp.coordinate = playerCenter[0]
+					deg, x, y = getDirection(temp.orientation)
+					temp.coordinate = (temp.coordinate[0] +  (x * 20 * math.cos(math.radians(deg ) ) ) , temp.coordinate[1] + (y * 20 * math.sin(math.radians(deg ) ) ) )
+					bullets.append(temp)
+				elif event.key == pygame.K_c:
+					temp = Bullet()
+					temp.firedBy = 1
+					temp.timeTravelled = 0
+					temp.orientation = playerOrientation[1]
+					temp.coordinate = playerCenter[1]
+					deg, x, y = getDirection(temp.orientation)
+					temp.coordinate = (temp.coordinate[0] +  (x * 20 * math.cos(math.radians(deg ) ) ) , temp.coordinate[1] + (y * 20 * math.sin(math.radians(deg ) ) ) )
+					bullets.append(temp)
+				elif event.key == pygame.K_b:
+					temp = Bullet()
+					temp.firedBy = 2
+					temp.timeTravelled = 0
+					temp.orientation = playerOrientation[2]
+					temp.coordinate = playerCenter[2]
+					deg, x, y = getDirection(temp.orientation)
+					temp.coordinate = (temp.coordinate[0] +  (x * 20 * math.cos(math.radians(deg ) ) ) , temp.coordinate[1] + (y * 20 * math.sin(math.radians(deg ) ) ) )
+					bullets.append(temp)
+				elif event.key == pygame.K_m:
+					temp = Bullet()
+					temp.firedBy = 3
+					temp.timeTravelled = 0
+					temp.orientation = playerOrientation[3]
+					temp.coordinate = playerCenter[3]
+					deg, x, y = getDirection(temp.orientation)
+					temp.coordinate = (temp.coordinate[0] +  (x * 20 * math.cos(math.radians(deg ) ) ) , temp.coordinate[1] + (y * 20 * math.sin(math.radians(deg ) ) ) )
+					bullets.append(temp)
 		
 		screen.fill(BLACK)
 
+		# print len(bullets)
+
+		for i in xrange(len(bullets)):
+			bullets[i].timeTravelled = bullets[i].timeTravelled + 1
+			deg, x, y = getDirection(bullets[i].orientation)
+			bullets[i].coordinate = (bullets[i].coordinate[0] +  (x * bulletDistance * math.cos(math.radians(deg ) ) ) , bullets[i].coordinate[1] + (y * bulletDistance * math.sin(math.radians(deg ) ) ) )
+			# b.coordinate = (int(b.coordinate[0]) , int(b.coordinate[1]));
+			# print bullets[i].coordinate
+			pygame.draw.circle(screen, WHITE, (int(bullets[i].coordinate[0]), int(bullets[i].coordinate[1])), 2, 0 )
+
 		for i in xrange(numberOfPlayers):
-			print i
-			deg = playerOrientation[i]
-			x = +1
-			y = -1
+			# print i
+			deg, x, y = getDirection(playerOrientation[i])
 
-			if playerOrientation[i] > 90 and playerOrientation[i] <= 180:
-				deg = 180 - playerOrientation[i]
-				x = -1
-				y = -1
-			elif playerOrientation[i] > 180 and playerOrientation[i]<= 270:
-				deg = playerOrientation[i] - 180
-				x = -1
-				y = +1
-			elif playerOrientation[i] > 270 and playerOrientation[i] <= 360:
-				deg = 360 - playerOrientation[i]
-				x = +1
-				y = +1
-
-			playerCenter[i] = (newPlayerCursor[i].get_rect().x,newPlayerCursor[i].get_rect().y)
+			playerCenter[i] = newPlayerCursor[i].get_rect().center
+			playerCenter[i] = (playerCenter[i][0] + playerCoordinate[i][0] , playerCenter[i][1] + playerCoordinate[i][1])
 			playerCoordinate[i] = (playerCoordinate[i][0] +  (x * unitDistance * math.cos(math.radians(deg ) ) ) , playerCoordinate[i][1] + (y * unitDistance * math.sin(math.radians(deg ) ) ) )
-			# playerCoordinate[0][1] = 
-
 			screen.blit(newPlayerCursor[i], playerCoordinate[i] )
 		for center in centers:
 			pygame.draw.circle(screen, GREY, center, 40, 0 )
