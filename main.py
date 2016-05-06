9789517405e6d1ed87276cc4cb754d12c59baa87
pakhandi <asimkprasad@gmail.com>
2016-05-06 20:08:40 +0530

    cleanUp

--- main.py
+++ main.py
@@ -12,27 +12,6 @@ import bullet
 pygame.init()
 pygame.display.init()
 
-
-def getDirection(orientation):
-	deg = orientation
-	x = +1
-	y = -1
-
-	if orientation > 90 and orientation <= 180:
-		deg = 180 - orientation
-		x = -1
-		y = -1
-	elif orientation > 180 and orientation<= 270:
-		deg = orientation - 180
-		x = -1
-		y = +1
-	elif orientation > 270 and orientation <= 360:
-		deg = 360 - orientation
-		x = +1
-		y = +1
-
-	return deg, x, y
-
 BULLET_DAMAGE = 10
 # 10 ** 18
 
