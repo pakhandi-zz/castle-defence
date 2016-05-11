072eb12992d4ec11472acc018a0b0cbb5c0deb8c
pakhandi <asimkprasad@gmail.com>
2016-05-11 14:42:17 +0530

    modular

--- bullet.py
+++ bullet.py
@@ -1,25 +1,5 @@
 import math
 
-def getDirection(orientation):
-	deg = orientation
-	x = +1
-	y = -1
-
-	if orientation > 90 and orientation <= 180:
-		deg = 180 - orientation
-		x = -1
-		y = -1
-	elif orientation > 180 and orientation<= 270:
-		deg = orientation - 180
-		x = -1
-		y = +1
-	elif orientation > 270 and orientation <= 360:
-		deg = 360 - orientation
-		x = +1
-		y = +1
-
-	return deg, x, y
-
 class Bullet:
 
 	timeTravelled = 0
